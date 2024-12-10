6637a51c4db0f180fd7c05c22627b7aa0f5e6921
SaittoXV <syed.abubakar73@gmail.com>
2024-12-10 16:09:13 +0800

    from flask import Flask, render_template import mysql.connector
    app = Flask(__name__)
    
    databaseConnection = mysql.connector.connect(
        host = "TestedJourneys.mysql.pythonanywhere-services.com",
        user = "TestedJourneys",
        password = "Test123!",
        database = "TestedJourneys$default"
    )
    
    myCursor = databaseConnection.cursor()
    myCursor.execute("SHOW TABLES")
    found = False
    for table in myCursor:
        print(table[0])
        print(found)
        if table[0] == "todo":
            found = True
            print("The table already exist.")
        else:
            print("The table was not found.")
    
    if found == False:
        myCursor.execute("CREATE TABLE todo (id INT NOT NULL, task VARCHAR(255), PRIMARY KEY(id))")
    else:
        print("Does not need to create table.")
    
    @app.route('/')
    def homePage():
        return render_template("index.html")
    
    @app.route('/Projects')
    def projectPage():
        return render_template("project.html")
    
    @app.route('/Projects/Task Tracker')
    def taskTrackerPage():
    
        return render_template("task_tracker.html")
    
    @app.route('/Games')
    def gamePage():
        return render_template("game.html")
    
    if __name__ == "__main__":
        app.run

--- app.py
+++ app.py
@@ -6,20 +6,32 @@ app = Flask(__name__)
 
 # Configuration settings for connecting to the MYSQL database
 databaseConnection = mysql.connector.connect(
-    host = "TestedJourneys.mysql.pythonanywhere-services.com"
-    user = "TestedJourneys"
-    password = "Test123!"
+    host = "TestedJourneys.mysql.pythonanywhere-services.com",
+    user = "TestedJourneys",
+    password = "Test123!",
     database = "TestedJourneys$default"
 )
 
-# 'MySQL' object from 'flask_mysql' extension is initialize with the flask app, creating a MYSQL connection
-mysql = MySQL(app)
+# Create Database Connection
+myCursor = databaseConnection.cursor()
 
-class Todo(mysql.model):
-    id = mysql.column(mysql.Integer,primary_key= True)
-    content = mysql.column(mysql.String(200), nullable = False)
-    
+# Check Table Exist
+found = False
+myCursor.execute("SHOW TABLES")
+for table in myCursor:
+    print(table[0])
+    print(found)
+    if table[0] == "todo":
+        found = True
+        print("The table already exist.")
+    else:
+        print("The table was not found.")
 
+# Create Table if does not exist
+if found == False:
+    myCursor.execute("CREATE TABLE todo (id INT NOT NULL, task VARCHAR(255), PRIMARY KEY(id))")
+else:
+    print("Does not need to create table.")
 
 @app.route('/')
 def homePage():
@@ -31,6 +43,7 @@ def projectPage():
 
 @app.route('/Projects/Task Tracker')
 def taskTrackerPage():
+
     return render_template("task_tracker.html")
 
 @app.route('/Games')
